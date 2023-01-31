86d64c7fb0be330cfb7d3507ce4b039a01ab2d92
Thirawee Thitiwattanakornsiri <51158745+kaho1910@users.noreply.github.com>
2023-01-31 20:39:43 +0700

    feat: week 4 | inclass example

---
+++ inclass-example/mytdd-fizzbuzz.py
@@ -0,0 +1,31 @@
+def test_fizzbuzz():
+    '''test fizz buzz'''
+    assert 1 == fizzbuzz(1)
+    assert 2 == fizzbuzz(2)
+    assert "Fizz" == fizzbuzz(3)
+    assert 4 == fizzbuzz(4)
+    assert "Buzz" == fizzbuzz(5)
+    assert "Fizz" == fizzbuzz(6)
+    assert 7 == fizzbuzz(7)
+    assert 8 == fizzbuzz(8)
+    assert "Fizz" == fizzbuzz(9)
+    assert "Buzz" == fizzbuzz(10)
+    assert 11 == fizzbuzz(11)
+    assert "Fizz" == fizzbuzz(12)
+    assert "Fizzbuzz" == fizzbuzz(15)
+    assert "Fizzbuzz" == fizzbuzz(30)
+    assert "Fizz" == fizzbuzz(33)
+
+    print("All test passed.")
+
+def fizzbuzz(num):
+    '''fizz buzz'''
+    if not num % 3 == 0 and num % 5 == 0:
+        return "Fizzbuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    return num
+
+test_fizzbuzz()
