e66253ac2a1aa282ab3152d243b99d2e9e3a1bb6
Thirawee Thitiwattanakornsiri <51158745+kaho1910@users.noreply.github.com>
2023-02-03 11:49:26 +0700

    feat: add LabParamiko example

---
+++ week4/example2-paramiko.py
@@ -0,0 +1,22 @@
+import time
+import paramiko
+
+USERNAME = "admin"
+PASSWORD = "cisco"
+
+devices_ip = ["172.31.101.4", "172.31.101.5", "172.31.101.6"]
+
+for ip in devices_ip:
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(hostname=ip, username=USERNAME, key_filename="/Users/kaho/Documents/KMITL/NPA/npa_rsa", look_for_keys=True)
+
+    commands = ["sh ip int br"]
+    for command in commands:
+        print("Executing {}".format(command))
+        stdin, stdout, stderr = client.exec_command(command)
+        print(stdout.read().decode())
+        print("Errors")
+        print(stderr.read().decode())
+        time.sleep(1)
+    client.close()
