5b44169120a95f152be42e5aa268088e1585e21f
Thirawee Thitiwattanakornsiri <51158745+kaho1910@users.noreply.github.com>
2023-02-16 10:37:18 +0700

    feat: update example2

--- week4/example2-paramiko.py
+++ week4/example2-paramiko.py
@@ -6,17 +6,20 @@ PASSWORD = "cisco"
 
 devices_ip = ["172.31.101.4", "172.31.101.5", "172.31.101.6"]
 
+commands = ["terminal length 0", "sh ip int br"]
+
 for ip in devices_ip:
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(hostname=ip, username=USERNAME, key_filename="/Users/kaho/Documents/KMITL/NPA/npa_rsa", look_for_keys=True)
 
-    commands = ["sh ip int br"]
-    for command in commands:
-        print("Executing {}".format(command))
-        stdin, stdout, stderr = client.exec_command(command)
-        print(stdout.read().decode())
-        print("Errors")
-        print(stderr.read().decode())
-        time.sleep(1)
+    print("Connecting to {} ...".format(ip))
+    with client.invoke_shell() as ssh:
+        print("Connecting to {} ...".format(ip))
+
+        for j in commands:
+            ssh.send(j + "\n")
+            time.sleep(1)
+            result = ssh.recv(1000).decode("ascii")
+            print(result)
     client.close()
