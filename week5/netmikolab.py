6e8ad431123b7d8d70ffc91ef6e0b77322df482c
Thirawee Thitiwattanakornsiri <51158745+kaho1910@users.noreply.github.com>
2023-02-18 21:42:16 +0700

    feat: update Lab Netmiko | configure interface

--- week5/netmikolab.py
+++ week5/netmikolab.py
@@ -14,15 +14,59 @@ def get_ip(device_params, intf):
             return words[1]
 
 if __name__ == "__main__":
-    device_ip = "172.31.101.4"
+    devices_ip = {
+        "R1": "172.31.101.4",
+        "R2": "172.31.101.5",
+        "R3": "172.31.101.6",
+    }
     username = "admin"
     password = "cisco"
 
     device_params = {
         "device_type": "cisco_ios",
-        "ip": device_ip,
+        "ip": "127.0.0.1",
         "username": username,
         "password": password
     }
 
-    print(get_ip(device_params, "G0/0"))
+    commands = {
+        "R1": [
+            "int G0/0",
+            "description Connect to G0/2 of S0",
+            "int G0/1",
+            "description Connect to G0/2 of S1",
+            "int G0/2",
+            "description Connect to G0/1 of R2",
+            "int G0/3",
+            "description Not Use"
+        ],
+        "R2":[
+            "int G0/0",
+            "description Connect to G0/3 of S0",
+            "int G0/1",
+            "description Connect to G0/2 of R1",
+            "int G0/2",
+            "description Connect to G0/1 of R3",
+            "int G0/3",
+            "description Not Use"
+        ],
+        "R3":[
+            "int G0/0",
+            "description Connect to G1/0 of S0",
+            "int G0/1",
+            "description Connect to G0/2 of R2",
+            "int G0/2",
+            "description Connect to WAN2",
+            "int G0/3",
+            "description Not Use"
+        ]}
+
+    for device in ["R1", "R2", "R3"]:
+        device_params["ip"] = devices_ip[device]
+        with ConnectHandler(**device_params) as ssh:
+            command = commands[device]
+            for i in command:
+                result = ssh.send_config_set(i)
+                print(result)
+
+        print(get_ip(device_params, "G0/0"))
